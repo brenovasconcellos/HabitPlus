a0bb7811b5a6a9cd417cd2df09e68e29eccd2d85
Breno Vasconcellos <dv.brenovasconcellos@gmail.com>
2022-05-17 11:03:49 -0300

    firts commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Breno Vasconcellos on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
